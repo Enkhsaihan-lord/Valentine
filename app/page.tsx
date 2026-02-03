31fd8cd3fd1c9bb08488701dc8c80c199ccf4913
Enkhsaihan-lord <enhsaihan.samdam@gmail.com>
2026-02-03 22:39:11 +0800

    urilga

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,112 @@
-import Image from "next/image";
-
-export default function Home() {
+"use client";
+import { Button } from "@/components/ui/button";
+import { Heart } from "lucide-react";
+import Snowfall from "react-snowfall";
+import { TypeAnimation } from "react-type-animation";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState, useEffect } from "react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import "./HeartRain.css";
+const Page = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <Snowfall />
+      <div className="justify-center">
+        <div className="w-full max-w-sm">
+          <div className="flex justify-center mb-4">
+            <Heart fill="red" stroke="red" size={50} />
+          </div>
+          <Card className="w-full max-w-sm border-pink-400 rounded-xl shadow-lg bg-opacity-80 bg-black/40 backdrop-blur-sm">
+            <CardHeader className="flex justify-center">
+              <CardAction>
+                <img
+                  src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGs3dzcxdDFiN3NjcXI2bHA2cGdpNzFrZ2c0emczNzdubjJsbmxxaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/ooVFPvRxN3uFnC9QTo/giphy.gif"
+                  alt=""
+                />
+              </CardAction>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="flex flex-col gap-6">
+                  <div className="grid gap-2">
+                    <TypeAnimation
+                      sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        "Хүндэт Хөөрхөн Энхжин таныг Valentine ",
+                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                      ]}
+                      wrapper="span"
+                      speed={50}
+                      style={{
+                        fontSize: "1.4rem",
+                        textAlign: "center",
+                        color: "pink",
+                      }}
+                      repeat={Infinity}
+                    />
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex-col gap-2">
+              <div className="flex gap-3">
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button className="bg-red-500 text-white hover:bg-red-600">
+                      татгалцах
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>why ve? </DialogTitle>
+                      <DialogDescription>zo zo</DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter></DialogFooter>
+                  </DialogContent>
+                </Dialog>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button className="bg-green-500 text-white hover:bg-green-600">
+                      Зөвшөөрөх
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Урилга хүлээж авсанд баярлаа.</DialogTitle>
+                      <DialogDescription>
+                        2сарын14 өдөр 11цагт багшийн дээд урд уулзая дулаан
+                        хувцаслаарай хөөрхнөө.
+                        <img
+                          className="flex justify-center"
+                          src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzJqZG82dGR3ZDZ0bXNiMjV6YWFsZzF6c2N3dng1ZjFtbTk1cTA2NiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/wIUQQ07BHzDry/giphy.gif "
+                          alt=""
+                        />
+                      </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter></DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              </div>
+            </CardFooter>
+          </Card>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
-}
+};
+export default Page;
